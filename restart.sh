59f9b6f8ae29aae6d8c511284994f07576a1646b
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-21 23:16:04 +0800

    init_container

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home
+mkdir -p tmp/node_default
 mkdir -p node/qdata/dd/keystore
 mkdir -p node/qdata/dd/geth
-
-cd /home/controlscript
-cp ui.sh /home/tmp
+cd /home/
+cp -r /node_default/. /tmp
