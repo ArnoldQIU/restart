09eabdd693b7f6c42a4298c8c7583f2e9950b1d1
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-22 03:28:21 +0800

    init_container

--- restart.sh
+++ restart.sh
@@ -16,8 +16,7 @@ GENERATE_KEY='#!/bin/bash
 	&& echo -ne "\n" | constellation-node --generatekeys=tm \
 	&& echo -ne "\n" | geth account new --password ./passwords.txt --keystore . \
 	&& mv UTC* key'
-echo "$GENERATE_KEY" > GENERATE_KEY.sh && chmod 755 GENERATE_KEY.sh && sh GENERATE_KEY.sh
-echo -ne '\n'
+echo "$GENERATE_KEY" > GENERATE_KEY.sh && chmod 755 GENERATE_KEY.sh 
 GENERATE_CONSTELLATION_START='#!/bin/bash
     set -u
     set -e
