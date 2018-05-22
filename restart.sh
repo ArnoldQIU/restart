bfe3e879fcc5dafcde9acc349c0a0ed3239a4864
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-22 20:35:56 +0800

    init-container

--- restart.sh
+++ restart.sh
@@ -41,7 +41,6 @@ echo "$GENERATE_CONSTELLATION_START" > constellation-start.sh && chmod 755 const
 echo "Generate permissioned-nodes.json in local"
 ENODE=$(cat /home/node/enode.key)
 COMBINE="enode://"$ENODE"@"$SERVICE_IP":21000?discport=0\"&\"raftport=50400"
-cd /home
-echo $COMBINE >> permissioned-nodes.json
+cd /home && echo $COMBINE >> permissioned-nodes.json
 cd /home/node && chmod 755 *.sh && ./stop.sh
 cd /home/node && ./raft-init.sh && ./raft-start.sh
