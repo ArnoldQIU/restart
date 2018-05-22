28bd16f313a4b59f4dc9ee4c186e2ca28ba4bbbe
邱珞Arnold <arrnoldc@hotmail.com>
2018-05-22 20:39:22 +0800

    init-container

--- restart.sh
+++ restart.sh
@@ -42,5 +42,5 @@ echo "Generate permissioned-nodes.json in local"
 ENODE=$(cat /home/node/enode.key)
 COMBINE="enode://"$ENODE"@"$SERVICE_IP":21000?discport=0\"&\"raftport=50400"
 cd /home && echo $COMBINE >> permissioned-nodes.json
-cd /home/node && chmod 755 *.sh && ./stop.sh
-cd /home/node && ./raft-init.sh && ./raft-start.sh
+## cd /home/node && chmod 755 *.sh && ./stop.sh
+## cd /home/node && ./raft-init.sh && ./raft-start.sh
